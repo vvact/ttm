2c0943cf1a3a2f258a6847b7522210eae068f41b
buda <vbm7752@gmail.com>
2024-10-07 18:18:28 +0300

    initial comit

--- media/urls.py
+++ media/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.blog.urls')),  
-    path('api-auth/', include('rest_framework.urls')),
+    # path('', include('apps.blog.urls')),
 ]
 
 # Serve media files during development
